bf8ece786e00c56c8665c222124bebecfc6d286b
AHMADQURAAN97 <ahmadqouraan@gmail.com>
2021-07-24 17:54:32 +0300

    err

---
+++ app.js
@@ -0,0 +1,96 @@
+'use strict';
+
+let containerForm = document.getElementById('container-form');
+let containerTable = document.getElementById('container-Table');
+let myForm = document.getElementById('myform');
+let tableEl = document.createElement('table');
+
+let cars =[];
+
+function car (cName,cModel,mYear) {
+
+    this.cName = cName;
+    this.cModel ="img/"+ cModel+".png";
+    this.mYear = mYear;
+    
+
+cars.push(this);
+saveTolocalStorage ();
+}
+
+
+
+car.prototype.render = function (){
+
+    
+
+  let trEl1 = document.createElement('tr');
+  
+
+  let tdName = document.createElement('td');
+  tdName.textContent = `Car name: ${this.cName}`;
+  trEl1.appendChild(tdName);
+
+  let displayImage = document.createElement('img');
+  displayImage.setAttribute ('src', this.cModel);
+  
+  
+  let tdImage = document.createElement('td');
+  tdImage.appendChild(displayImage);
+  trEl1.appendChild(tdImage);
+  
+  let trEl2 = document.createElement('tr');
+
+  let tdYear = document.createElement('td');
+  tdYear.textContent = `Model Car : ${this.mYear}`;
+trEl2.appendChild(tdYear);
+
+tableEl.appendChild(trEl1);
+tableEl.appendChild(trEl2);
+containerTable.appendChild(tableEl);
+}
+
+
+myForm.addEventListener('submit', showCar);
+
+function showCar (event) {
+
+    event.preventDefault();
+
+    let cName = event.target.cName.value;
+    let cModel = event.target.cModel.value;
+    let mYear = event.target.mYear.value;
+
+    let newCar = new car (cName,cModel,mYear);
+    newCar.render();
+
+
+}
+
+
+
+
+function saveTolocalStorage (){
+
+    let saveLocal =JSON.stringify(cars);
+    localStorage.setItem('Car',saveLocal)
+
+}
+
+function readFromLocalStorage (){
+
+    let stringObj = localStorage.getItem('Car');
+    let normalObj = JSON.parse(stringObj);
+    if (normalObj !== null) {
+        for ( let i=0 ; i < normalObj.length ; i++){
+
+            new car (normalObj[i].cName, normalObj[i].cModel,normalObj[i].mYear);
+            
+            cars[i].render();
+        }
+
+        
+    }
+    
+}
+readFromLocalStorage ();
